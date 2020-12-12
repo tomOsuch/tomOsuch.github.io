c7425c29b65723f1c5d61b07e04f6c5042442a7a
tosuch <tomasz.osuch@gmail.com>
2020-12-12 19:51:35 +0100

    Update project

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
-
-
-
     const apiRoot = 'https://mighty-citadel-78297.herokuapp.com/v1/task/';
     const trelloApiRoot = 'https://mighty-citadel-78297.herokuapp.com/v1/trello/';
     const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
-    const tasksContainer = $('[data-tasks-container]');
+    const $tasksContainer = $('[data-tasks-container]');
 
     var availableBoards = {};
     var availableTasks = {};
